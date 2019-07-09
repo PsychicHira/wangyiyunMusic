256d11bffa41c6cb3b0b57d5ff5860486a3bb8e1
hira_glb <psychichira@163.com>
2019-07-09 18:16:42 +0800

    .

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,16 +6,16 @@ Page({
     personalizedMv:Array,
     mvId:[],
     mvUrl:[],
-    newSongFlag:true
+    newSongFlag:true,
+    newDisc:[],
+    newSong:[]
   },
-  // 
   toDisc:function(){
     this.setData({
       newSongFlag : true
     })
   },
   toSong:function(){
-    console.log(2)
     this.setData({
       newSongFlag : false
     })
@@ -24,6 +24,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+
     //获取轮播图
     api.banner().then(res=>{
       this.setData({
@@ -32,33 +33,29 @@ Page({
     }).catch(err=>{
       console.log(err)
     })
+
     //获取推荐歌单
     api.personalized().then(res=>{
       this.setData({
         //推荐歌单只有6个位置，所以截取前6个
         personalized:res.result.splice(0,6)
       })
-      // console.log(this.data.personalized)
     }).catch(err=>{
       console.log(err)
     })
+
     //获取推荐mv（云村精选）
     api.personalizedMv().then(res=>{
-      // console.log(res)
       this.setData({
         personalizedMv:res.result.splice(0,6)
       })
-      // console.log(this.data.personalizedMv)
-      
         //获取mvId
         this.data.personalizedMv.forEach(element => {
           this.data.mvId.push(element.id)
         })
-        //console.log(this.data.mvId);  //有了Id
-        
     }).then(()=>{
-      // 获取mvUrl
-        // 遍历id，发送请求，把url存进数组
+        // 获取mvUrl
+          // 遍历id，发送请求，把url存进数组
       let arr=[]
         this.data.mvId.forEach((element,index) => {
           api.getMvUrl({id:element}).then(res=>{
@@ -70,36 +67,34 @@ Page({
             console.log(err)
           })
         })
-        // console.log(arr)
     }).catch(err=>{
       console.log(err)
     })
-    
-    
-    // console.log(this.data.personalizedMv)
 
-    //获取mvUrl
-    // this.data.personalizedMv.forEach(element => {
-    //   this.data.mvId.push(element.id)
-    //   // console.log(element);
-    // })
-    // console.log(this.data.personalizedMv.length)
-    
-    // api.getMvUrl().then(res=>{
-    //   this.setData({
-    //   })
-    //   // console.log(this.data.personalizedMv)
-    // }).catch(err=>{
-    //   console.log(err)
-    // })
-  },
+    //获取新碟
+    api.getNewDisc({
+        offset:0,
+        limit:3
+    }).then(res=>{
+      this.setData({
+        newDisc:res.albums
+      })
+    })
+
+        //获取新歌
+    api.getNewSong().then(res=>{
+      console.log(res)
+      this.setData({
+        newSong:res.data.splice(0,3)
+      })
+    })
+
+},
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
-    // console.log(this.data.personalizedMv[0]);
-    
+
   },
 
   /**
--- utils/api.js
+++ utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/"
+const BASE_URL = "http://192.168.0.2:3000/"
 const REQUEST = (requetUrl,data)=>{
     return new Promise((res,rej)=>{
         let url = BASE_URL + requetUrl
@@ -18,16 +18,28 @@ const REQUEST = (requetUrl,data)=>{
 }
 
 module.exports = {
+    //获取轮播
     banner:(data)=>{
         return REQUEST('banner',data)
     },
+    //获取推荐歌单
     personalized:(data)=>{
         return REQUEST('personalized',data)
     },
+    //获取推荐mv
     personalizedMv:(data)=>{
         return REQUEST('personalized/mv',data)
     },
+    //获取mv的url（需要mv的id）
     getMvUrl:(data)=>{
         return REQUEST('mv/url',data)
+    },
+    //获取新碟信息
+    getNewDisc:(data)=>{
+        return REQUEST('top/album',data)
+    },
+    //获取新歌信息
+    getNewSong:(data)=>{
+        return REQUEST('top/song',data)
     }
 }
