c5ee7f1142606f4322593e2e8ab48c9e31d051d8
hira_glb <psychichira@163.com>
2019-07-11 23:41:20 +0800

    视频页获取接口数据

--- components/videoBox/videoBox.js
+++ components/videoBox/videoBox.js
@@ -22,18 +22,25 @@ Component({
    */
   methods: {
     getMvUrl:function(e){
-      console.log(e)
-      this.data.mvId.forEach(element=>{
+      //获取触发播放事件的视频是mvId里面的第几个，得到mv的ID
+      let id = this.data.mvId[e.target.dataset.index]
+      //获取mv的地址
         api.getMvUrl({
-          id:element
+          id:id
         }).then(res=>{
-          // console.log(res)
+          //把整个视频数组取出来
+          let obj = this.data.video
+          //给当前这个mv对象增加url
+          obj[e.target.dataset.index].url = res.data.url
+          //把video对象重新覆盖，达到双向绑定的目的，让页面可以播放视频
+          this.setData({
+            video : obj
+          })
         })
-      })
       
     }
   },
-  attached:function(){
+  ready:function(){
     let arr=[]
     this.data.video.forEach(element => {
       arr.push(element.id)
@@ -41,7 +48,7 @@ Component({
     this.setData({
       mvId:arr
     })
-    console.log(9090)
+    // console.log(this.data.mvId)
   },
 
 })
