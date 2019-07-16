fb001b1b88e1e55dfbe3cc3268023aea85f79e55
hira_glb <psychichira@163.com>
2019-07-16 22:15:27 +0800

    优化了video子组件videoBox中的方法

--- components/videoBox/videoBox.js
+++ components/videoBox/videoBox.js
@@ -23,6 +23,7 @@ Component({
   methods: {
     //点击播放，获取mv地址
     getMvUrl:function(e){
+      
       //获取触发播放事件的视频是mvId里面的第几个，得到mv的ID
             // console.log(e.target.dataset.index)
       let id = this.data.mvId[e.target.dataset.index]
@@ -39,45 +40,59 @@ Component({
             video : obj
           })
         })
+
+
+    },
+    //把video中的id提取出来存入变量mvId
+    getMvId:function(){
+      let arr=[]
+      this.data.video.forEach(element => {
+        arr.push(element.id)
+      })
+      this.setData({
+        mvId:arr
+      })
+    },
+        //使用mvId获取mv数据信息
+    gerMvInfo:function(){
+      let mvInfo = []
+      let obj=[]
+      obj = this.data.video
+      this.data.mvId.forEach((element,index)=>{
+  
+        api.getMvInfo({
+          mvid:element
+        }).then(res=>{
+          mvInfo.push(res.data)
+          //res.data.commentCount
+          //res.data.likeCount
+          //res.data.artists.name
+        }).then((res)=>{
+          // console.log("mvInfo",mvInfo)
+          // console.log("obj",obj[index])
+          obj[index].likeCount = mvInfo[index].likeCount
+          obj[index].commentCount = mvInfo[index].commentCount
+          obj[index].artists.name = mvInfo[index].artists.name
+          this.setData({
+            video:obj
+          })
+        })
+  
+      })
     }
+
+    
   
   },
   ready:function(){
-    //把video中的id提取出来存入变量mvId
-    let arr=[]
-    this.data.video.forEach(element => {
-      arr.push(element.id)
-    })
-    this.setData({
-      mvId:arr
-    })
+    
+    this.getMvId()
+    this.gerMvInfo()
+
+
     // console.log(this.data.video)
   
-    //使用mvId获取mv数据信息
-    let mvInfo = []
-    let obj=[]
-    obj = this.data.video
-    this.data.mvId.forEach((element,index)=>{
 
-      api.getMvInfo({
-        mvid:element
-      }).then(res=>{
-        mvInfo.push(res.data)
-        //res.data.commentCount
-        //res.data.likeCount
-        //res.data.artists.name
-      }).then((res)=>{
-        // console.log("mvInfo",mvInfo)
-        // console.log("obj",obj[index])
-        obj[index].likeCount = mvInfo[index].likeCount
-        obj[index].commentCount = mvInfo[index].commentCount
-        obj[index].artists.name = mvInfo[index].artists.name
-        this.setData({
-          video:obj
-        })
-      })
-
-    })
     
     // new Promise((res,rej)=>{
     //   console.log(789)
@@ -107,6 +122,20 @@ Component({
     // this.setData({
     //   video:obj
     // })
+
+
+      new Promise((res,rej)=>{
+        if(true){
+          res("zhen")
+        }else{
+          rej("jia")
+        }
+      }).then(res=>{
+        console.log(res)
+      }).then(res=>{
+        console.log(123)
+      })
+
 
   }
 })
