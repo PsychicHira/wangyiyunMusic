07a8c5d5af3b16b52ff42144f7513929094beeec
hira_glb <psychichira@163.com>
2019-08-22 09:47:50 +0800

    完成输入密码页

--- pages/login/password/password.js
+++ pages/login/password/password.js
@@ -1,24 +1,61 @@
-// pages/login/password/password.js
+// pages/login/phone/phone.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    redColor: false,
+    phoneNum: '',
+    isClick: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let phoneNum= wx.getStorageSync('phoneNum')
-    console.log(phoneNum)
-
     wx.setNavigationBarTitle({
       title: '手机号登陆'
     })
   },
+
+  /**
+   * 自定义函数
+   */
+  //改变button颜色
+  changeColor: function (e) {
+    if (e.detail.value) {
+      this.setData({
+        redColor: true,
+        isClick: false,
+        phoneNum: e.detail.value
+      })
+    } else {
+      this.setData({
+        redColor: false,
+        isClick: true,
+        phoneNum: e.detail.value
+      })
+    }
+  },
+  //下一步跳转
+  next: function () {
+    if (this.data.phoneNum.length < 11) {
+      wx.showToast({
+        title: '手机号不正确',
+        duration: 2000
+      })
+      return
+    }
+    wx.setStorage({
+      key: 'phoneNum',
+      data: this.data.phoneNum
+    })
+    wx.navigateTo({
+      url: '/pages/login/password/password'
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
